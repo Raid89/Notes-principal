5e9368b4ca633f632c743ee15a86306817bc7515
Raid89 <nicolas.cr0603@gmail.com>
2023-06-10 17:42:01 -0500

    configuracion inicial layout y lenguajes

--- src/app/modules/notes/notes.module.ts
+++ src/app/modules/notes/notes.module.ts
@@ -4,10 +4,6 @@ import { NotesRoutingModule } from './notes-routing.module';
 import { NotesItemComponent } from './pages/notes-item/notes-item.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { NewNoteFormComponent } from './pages/new-note-form/new-note-form.component';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { RouterModule } from '@angular/router';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { NotesGalleryComponent } from './pages/notes-gallery/notes-gallery.component';
 
 
